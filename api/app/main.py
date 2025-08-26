838b5d62466cf2a8b49c2ee1dd81e07d2b9dab6f
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-08-26 23:02:42 +0200

    feat(api): add cors middleware

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,18 @@
 from app.routes.product import router as product_router
 from app.routes.stock import router as stock_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5174"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(product_router, prefix="/products", tags=["/products"])
 app.include_router(stock_router, prefix="/stock", tags=["/stock"])
