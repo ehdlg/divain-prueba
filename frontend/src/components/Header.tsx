266d3f0e6a9d8c14ac8387d6b2e049fdd979647f
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-08-26 23:30:05 +0200

    feat(header): rename Home -> Inventario

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <nav className='p-4 bg-slate-800 flex gap-4 text-xl hover:text-white border-b border-slate-700 shadow'>
-      <Link to='/'>Home</Link>
+      <Link to='/'>Inventario</Link>
       <Link to='/stock'>Movimiento de stock</Link>
     </nav>
   );
