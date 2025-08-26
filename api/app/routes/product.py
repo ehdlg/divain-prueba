0ba1067374ad7e96416708523032b4708fc7b623
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-08-26 20:04:45 +0200

    feat(routes): add field validator for stock UpdateProduct type

--- api/app/routes/product.py
+++ api/app/routes/product.py
@@ -3,16 +3,8 @@ from uuid import UUID
 from app.middlewares.main import ProductService, get_product_service
 from app.routes.typings import UpdateProduct
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import ValidationError
 
 router = APIRouter()
-
-
-async def validate_product_data(product: UpdateProduct):
-    if product.stock is not None and product.stock < 0:
-        raise HTTPException(status_code=422, detail="Stock cannot be lesser than 0")
-
-    return product
 
 
 @router.get("")
@@ -27,7 +19,7 @@ async def get_products(
 @router.patch("/{id}")
 async def update_product(
     id: UUID,
-    product: UpdateProduct = Depends(validate_product_data),
+    product: UpdateProduct,
     product_service: ProductService = Depends(get_product_service),
 ):
     try:
--- api/app/routes/typings.py
+++ api/app/routes/typings.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class UpdateProduct(BaseModel):
@@ -8,3 +8,10 @@ class UpdateProduct(BaseModel):
     ean13: Optional[str] = None
     stock: Optional[int] = None
     name: Optional[str] = None
+
+    @field_validator("stock")
+    def stock_must_be_non_negative(cls, v):
+        if v is not None and v < 0:
+            raise ValueError("Stock cannot be less than 0")
+
+        return v
