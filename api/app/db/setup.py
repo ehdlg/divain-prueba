8f02495c8a25864bc384f6354372e16b98cf59ce
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-08-26 15:09:01 +0200

    feat(db): add setup script to create db schemas

---
+++ api/app/db/setup.py
@@ -0,0 +1,18 @@
+from app.db.main import engine
+from app.db.models import Product, StockMovement, User  # noqa: F401
+from sqlmodel import SQLModel
+
+
+def create_db_and_tables():
+    try:
+        engine.echo = False
+
+        SQLModel.metadata.create_all(engine)
+
+        print("Tables created successfully.")
+    except Exception as e:
+        print(f"Error creating tables: {e}")
+
+
+if __name__ == "__main__":
+    create_db_and_tables()
