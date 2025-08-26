72926b4ee12a2253aad9225f4ca55b9b77d46c05
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-08-26 16:13:11 +0200

    feat(repository): add initial StockMovement repository

---
+++ api/app/repositories/stock_movement.py
@@ -0,0 +1,22 @@
+from app.db.models import StockMovement
+from sqlmodel import Session, select
+
+
+class StockMovementRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def create(self, stock_movement: StockMovement):
+        self.session.add(stock_movement)
+        self.session.commit()
+
+        self.session.refresh()
+
+        return stock_movement
+
+    def get_all(self):
+        statement = select(StockMovement)
+
+        stock_movements = self.session.exec(statement).all()
+
+        return stock_movements
