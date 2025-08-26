2f170dad3122c0880a46ff6efc29410a8e1271a4
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-08-26 15:27:09 +0200

    feat(routes): add get_all method for /products GET endpoint

--- api/app/routes/product.py
+++ api/app/routes/product.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from app.middlewares.main import ProductRepository, get_product_repository
+from fastapi import APIRouter, Depends
 
 router = APIRouter()
 
 
-@router.get("/")
-async def get_products():
-    products = [f"product {i}" for i in range(10)]
+@router.get("")
+async def get_products(
+    product_repo: ProductRepository = Depends(get_product_repository),
+):
+    products = product_repo.get_all()
 
     return products
