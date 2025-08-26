87317601c0e90fcbdbb50d15a0b1c7520ade0726
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-08-26 16:01:45 +0200

    feat(routes): add PATCH endpoint to update product

--- api/app/repositories/product.py
+++ api/app/repositories/product.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from app.db.models import Product
 from sqlmodel import Session, select
 
@@ -6,9 +8,27 @@ class ProductRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def get_all(self):
+    def get_all(self) -> list[Product]:
         statement = select(Product)
 
         products = self.session.exec(statement).all()
 
         return products
+
+    def get_one(self, id: UUID | str) -> Product | None:
+        if isinstance(id, str):
+            id = UUID(id)
+
+        statement = select(Product).where(Product.id == id)
+        product = self.session.exec(statement).one_or_none()
+
+        return product
+
+    def update(self, product: Product, data: dict) -> Product:
+        product.sqlmodel_update(data)
+
+        self.session.add(product)
+        self.session.commit()
+        self.session.refresh(product)
+
+        return product
