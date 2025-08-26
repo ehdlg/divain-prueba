90fcd875f1c4836623f563e906d53587cc01a0bc
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-08-26 16:01:32 +0200

    feat: add method for Product service and repo to update product

--- api/app/routes/product.py
+++ api/app/routes/product.py
@@ -1,4 +1,7 @@
+from uuid import UUID
+
 from app.middlewares.main import ProductService, get_product_service
+from app.routes.typings import UpdateProduct
 from fastapi import APIRouter, Depends
 
 router = APIRouter()
@@ -11,3 +14,16 @@ async def get_products(
     products = product_service.get_all()
 
     return products
+
+
+@router.patch("/{id}")
+async def update_product(
+    id: UUID,
+    product: UpdateProduct,
+    product_service: ProductService = Depends(get_product_service),
+):
+    update_data = product.model_dump(mode="json", exclude_unset=True)
+
+    updated_product = product_service.update(id, update_data)
+
+    return updated_product
--- api/app/services/product.py
+++ api/app/services/product.py
@@ -1,4 +1,6 @@
-from app.repositories.product import ProductRepository
+from uuid import UUID
+
+from app.repositories.product import Product, ProductRepository
 
 
 class ProductService:
@@ -7,3 +9,16 @@ class ProductService:
 
     def get_all(self):
         return self.repository.get_all()
+
+    def get_one(self, id: UUID):
+        return self.repository.get_one(id)
+
+    def update(self, id: UUID, data: dict) -> Product:
+        product = self.repository.get_one(id)
+
+        if not product:
+            raise ValueError(f"Product with id {id} not found.")
+
+        updated_product = self.repository.update(product, data)
+
+        return updated_product
